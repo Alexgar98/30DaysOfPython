d4c6a7a3c520c1574b9055303746cb730f7c1d42
Alex <92014559+Alexgar98@users.noreply.github.com>
2024-07-01 23:49:27 +0200

    Initial commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,45 @@
+import math
+import sys
+
+#Level 2. Level 1 was basically this but in terminal
+
+print(sys.version) #Python version. Would be py --version in terminal
+
+print(3+4) #Addition
+print(3-4) #Substraction
+print(3*4) #Multiplication
+print(3%4) #Modulus
+print(3/4) #Division
+print(3**4) #Exponential
+print(3//4) #Floor division
+
+print('Alex') #Name
+print("Garcia") #Family name
+print("Spain") #Country
+print("I am enjoying 30 days of python") #Stuff
+
+print(type(10)) #Checking data types
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Alex'))
+print(type("Garcia"))
+print(type("Spain"))
+
+#Level 3
+
+print(type(1)) #Integer
+print(type(1.4)) #Float
+print(type(1+4j)) #Complex
+print(type("Hola que tal")) #String
+print(type(True)) #Boolean
+print(type(["Soy", "una", "lista"])) #List
+print(type(("Soy", "una", "tupla"))) #Tuple
+print(type({"Soy", "un", "set"})) #Set
+print(type({'prueba':'pruebita'})) #Dictionary
+
+print((10+8j) - (2+3j)) #As complex
+print(math.sqrt((2-10)**2 + (3-8)**2)) #As Cartesian coordinates
+
+#Would've normally used variables for this but oh welp. That's for tomorrow :3
